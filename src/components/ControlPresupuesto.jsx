3dbe433ae62565299d40ef752a4a1842e29890fe
Gustavo Duarte <gustavoduarte199610@hotmail.com>
2023-10-01 13:08:13 -0300

    WebSite OK

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -4,7 +4,7 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 
-const ControlPresupuesto = ({ gastos, presupuesto }) => {
+const ControlPresupuesto = ({ gastos, setGastos, presupuesto, setPresupuesto, setIsValidPresupuesto }) => {
 
     const [porcentaje, setPorcentaje] = useState(0);
     const [disponible, setDisponible] = useState(0);
@@ -29,6 +29,15 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
         })
     }
 
+    const handleResetApp = () => {
+        const resultado = confirm('Deseas reiniciar presupuestos y gastos?')
+        if(resultado){
+            setGastos([])
+            setPresupuesto(0)
+            setIsValidPresupuesto(false)
+        }
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>
@@ -43,6 +52,13 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
             />
         </div>
         <div className='contenido-presupuesto'>
+            <button 
+                className='reset-app'
+                type='buttton'
+                onClick={handleResetApp}
+            >
+                Resetar App
+            </button>
             <p>
                 <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
             </p>
