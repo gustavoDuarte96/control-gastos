5556da9ece33bea3bebb7ebcf86f320fc30a3077
Gustavo Duarte <gustavoduarte199610@hotmail.com>
2023-10-01 12:57:14 -0300

    Marcar presupuesto superado OK

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -34,8 +34,8 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
         <div>
             <CircularProgressbar 
                 styles={buildStyles({
-                    pathColor: '#3B82F6',
-                    textColor: '#3B82F6',
+                    pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
+                    textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
                     trailColor: '#F5F5F5',
                 })}
                 value={porcentaje}
@@ -47,7 +47,7 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
                 <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
             </p>
 
-            <p>
+            <p className={`${disponible < 0 ? 'negativo' : '' }`}>
                 <span>Disponible: </span> {formatearCantidad(disponible)}
             </p>
 
